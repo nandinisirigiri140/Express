0a4061434a7d0bb415ab754fa14558a86557773e
Gonuguntl <ajay.naidu116@gmail.com>
2026-01-02 19:38:34 +0530

    done

---
+++ serverr.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res) => {
+    res.send('Hello World! welcome to my Express API');
+});
+app.listen(3000,() => {
+    console.log('server running on http://localhost:3000');
+});
